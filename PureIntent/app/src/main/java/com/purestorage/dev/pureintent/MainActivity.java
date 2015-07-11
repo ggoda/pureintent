73a42bdfa02116b34ee59a80f0951e8dbe2a7eaa
Azaria Zornberg <azaria.zornberg@azaria.zornberg-mbp>
2015-07-11 15:04:40 -0700

    permissions stuff

--- PureIntent/app/src/main/java/com/purestorage/dev/pureintent/MainActivity.java
+++ PureIntent/app/src/main/java/com/purestorage/dev/pureintent/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -139,6 +140,9 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 }catch(Exception e){
+                    System.out.println(e);
+                    System.err.println(e);
+                    Log.e(e.toString(), e.toString());
 
                 }
             }
@@ -277,7 +281,7 @@ public class MainActivity extends AppCompatActivity {
                         emergencyReported();
                     }
                 });
-        }
+            }
         });
     }
 }
