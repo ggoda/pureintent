f00571c51f7b4e05a64a202f060453d79f914f7e
Victor Ying <victor.ying@purestorage.com>
2015-07-11 12:01:33 -0700

    Possibly fixed button bug

--- PureIntent/app/src/main/java/com/purestorage/dev/pureintent/MainActivity.java
+++ PureIntent/app/src/main/java/com/purestorage/dev/pureintent/MainActivity.java
@@ -1,6 +1,9 @@
 package com.purestorage.dev.pureintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,10 +11,22 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button emergencyButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        emergencyButton = (Button)findViewById(R.id.EmergencyButton);
+        emergencyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                startActivity(i);
+            }
+        });
     }
     @Override
     public void onResume ()
---
+++ PureIntent/app/src/main/java/com/purestorage/dev/pureintent/SettingsActivity.java
@@ -0,0 +1,9 @@
+package com.purestorage.dev.pureintent;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by victor.ying on 7/11/15.
+ */
+public class SettingsActivity extends AppCompatActivity {
+}
